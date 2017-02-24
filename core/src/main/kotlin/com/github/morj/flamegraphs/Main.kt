fdd79ab27109e9e1b19d4dbc44d1091d05b7cbe9
Pavel Nikitin <pavel.nikitin@jetbrains.com>
2017-02-24 16:53:42 +0100

    minor

--- core/src/main/kotlin/com/github/morj/flamegraphs/Main.kt
+++ core/src/main/kotlin/com/github/morj/flamegraphs/Main.kt
@@ -10,7 +10,7 @@ val usage = """
 
   Usage:
   1. generate csv dump via `java -jar -Dexport.call.tree.cpu -Dexport.csv ~/yjp/lib/yjp.jar -export ~/Snapshots/abc.snapshot ~/TempFolderName/`
-  2. run this program via `java -jar flamegraphs.jar [TempFolderName] [ThreadName] > /path/to/stacks-dump.txt`
+  2. run this program via `java -jar yjp-flamegraphs.jar [TempFolderName] [ThreadName] > /path/to/stacks-dump.txt`
   3. generate flame graph with https://github.com/brendangregg/FlameGraph
      `./flamegraph.pl --width=1900 /path/to/stacks-dump.txt > /path/to/stacks.svg`
 """
